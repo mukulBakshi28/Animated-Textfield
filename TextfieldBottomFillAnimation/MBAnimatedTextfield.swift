90e2d6742698cae4e94f1c6bd5a5bad88d51364f
Mukul Bakshi <mukul.bakshi@paytm.com>
2019-05-15 23:21:48 +0530

    [#Developer Animated Textfield Layer Animation completed]

---
+++ TextfieldBottomFillAnimation/MBAnimatedTextfield.swift
@@ -0,0 +1,77 @@
+//
+//  MBAnimatedTextfield.swift
+//  TextfieldBottomFillAnimation
+//
+//  Created by Mukul Bakshi on 15/05/19.
+//  Copyright © 2019 Mukul Bakshi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MBAnimatedTextfield:UITextField,UITextFieldDelegate {
+    
+    enum AnimationPosition {
+        case Start
+        case Center
+    }
+    
+    weak var mbDelegate:MBTextfielDelegates?
+    var animationPos:AnimationPosition = .Start
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.delegate = self
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        mbDelegate?.mbTextFieldDidBeginEditing(txtField: textField)
+        addAnimatingLayer()
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mbDelegate?.mbTextFieldDidEndEditing(txtField: textField)
+        removeAnimatingLayer()
+    }
+}
+
+//Animating Layer
+extension MBAnimatedTextfield {
+    
+    func addAnimatingLayer() {
+        let lineLayer = CALayer()
+        let pointVal = animationPos == .Start ? CGPoint(x: 0, y: 0) : CGPoint(x: 0.5, y: 0.5)
+        lineLayer.anchorPoint = pointVal
+        lineLayer.frame = CGRect(x: 0, y: frame.size.height - 1, width: frame.size.width, height: 2)
+        lineLayer.backgroundColor = UIColor.red.cgColor
+        layer.addSublayer(lineLayer)
+        let bAnimation = CABasicAnimation()
+        bAnimation.keyPath = "bounds"
+        bAnimation.fromValue = NSValue(cgRect: CGRect(x: 0, y: frame.size.height - 1, width: 0, height: 2))
+        bAnimation.toValue = NSValue(cgRect: CGRect(x: 0, y: frame.size.height - 1, width: frame.size.width, height: 2))
+        bAnimation.duration = 0.2
+        lineLayer.add(bAnimation, forKey: "layerAnimation")
+    
+    }
+    func removeAnimatingLayer() {
+        for subLayers in layer.sublayers!  {
+            subLayers.removeFromSuperlayer()
+        }
+    }
+}
+
+protocol MBTextfielDelegates: class {
+    func mbTextFieldDidBeginEditing(txtField:UITextField)
+    func mbTextFieldDidEndEditing(txtField:UITextField)
+}
+
+extension MBTextfielDelegates {
+    func mbTextFieldDidBeginEditing(txtField:UITextField) {}
+    func mbTextFieldDidEndEditing(txtField:UITextField) {}
+}
+
---
+++ TextfieldBottomFillAnimation/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  TextfieldBottomFillAnimation
+//
+//  Created by Mukul Bakshi on 15/05/19.
+//  Copyright © 2019 Mukul Bakshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var myTxtField:MBAnimatedTextfield = {
+        let txtFld = MBAnimatedTextfield(frame: CGRect(x: 0, y: 0, width: 200, height: 30))
+        txtFld.borderStyle = .bezel
+        txtFld.placeholder = "Enter the Name"
+        return txtFld
+    }()
+    
+    //MARK: LifeCycleMethod
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myTxtField.center = view.center
+        myTxtField.animationPos = .Center
+        myTxtField.mbDelegate = self
+        view.addSubview(myTxtField)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+}
+
+extension ViewController:MBTextfielDelegates {
+}
+
